af8281772defb85a623ea037578cf87ce340da55
BruceHsiao <55333870+BrusaHsiao@users.noreply.github.com>
2019-09-21 14:50:30 -0700

    Update circle(runningaround)andsquare(jumpingupanddownchangingsize).js

--- circle(runningaround)andsquare(jumpingupanddownchangingsize).js
+++ circle(runningaround)andsquare(jumpingupanddownchangingsize).js
@@ -5,6 +5,8 @@ function setup() {
 var ballX = 50
 var ballSpeed = 20
 
+var boomX = 20
+var boombie = 2
 
 var ballX2 = 50
 var ballSpeed2 = 3
@@ -12,12 +14,13 @@ var ballSpeed2 = 3
 function draw() {
   background(220);
   fill(0, 255, 255);
-  ellipse(ballX, ballX2, 50);
+  ellipse(ballX, ballX2, boomX);
   fill(255, 0, 255);
   rect(ballX2, ballX, mouseY, mouseX)
 
   ballX = ballX + ballSpeed;
   ballX2 = ballX2 + ballSpeed2
+  boomX = boomX + boombie
 
   if (ballX >= 400) {
     ballSpeed = ballSpeed * -1
@@ -29,6 +32,12 @@ function draw() {
 
   if (ballX <= 0) {
     ballSpeed = ballSpeed * -1
+  }
+  if (boomX >= 200) {
+    boombie = boombie * -1
+  }
+  if (boomX <= 0) {
+    boombie = boombie * -1
   }
 
   if (ballX2 <= 0) {
